8fc7fad618b30b9ef1197e90fd6db2d0ebe0e2aa
Everton Bezerra <evertonbezerra_93@hotmail.com>
2020-05-16 12:53:14 -0300

    creating project on github

---
+++ lambda.js
@@ -0,0 +1,22 @@
+const awsServerlessExpress = require("aws-serverless-express");
+const express = require("express");
+ 
+const app = express();
+ 
+app.get("/user", (req, res) => {
+  res.json({
+    message: "user route"
+  });
+});
+ 
+app.get("/catalog", (req, res) => {
+  res.json({
+    message: "catalog route"
+  });
+});
+ 
+const server = awsServerlessExpress.createServer(app);
+ 
+exports.handler = (event, context) => {
+  awsServerlessExpress.proxy(server, event, context);
+};
